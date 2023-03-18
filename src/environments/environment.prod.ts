fed78c2ad818ca36e8dfcb22c4578d528d2c7a1d
Anthony Kazyaka <ask.anthony@hotmail.com>
2023-03-18 01:35:08 -0400

    Uncomment firebase config in the prod environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyBioQlLJ7Ggml_DoWdFyqyehTNO8GmxD4U",
+    authDomain: "lawnscapers-61d52.firebaseapp.com",
+    projectId: "lawnscapers-61d52",
+    storageBucket: "lawnscapers-61d52.appspot.com",
+    messagingSenderId: "711007224272",
+    appId: "1:711007224272:web:b4093647850de5a34ff9d5"
+    }
 };
