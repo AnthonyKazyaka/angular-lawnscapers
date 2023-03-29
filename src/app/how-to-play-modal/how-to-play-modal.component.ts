8ae8bbe379c21ec9e6a1adc84eba9d4ef6248de5
Anthony Kazyaka <anthony.kazyaka@gmail.com>
2023-03-29 01:21:13 -0400

    How to Play & Help Modals (#8)
    * Add How To Play component with initial code
    
    * Add additional logic & content to How to Play
    
    * Add help modal component files
    
    * Adjust angular.json budgets and the help modal contents

---
+++ src/app/how-to-play-modal/how-to-play-modal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-how-to-play-modal',
+  templateUrl: './how-to-play-modal.component.html',
+  styleUrls: ['./how-to-play-modal.component.css']
+})
+export class HowToPlayModalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
