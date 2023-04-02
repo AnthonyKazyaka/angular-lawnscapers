593540273e1d240ce64f33d2c0dae9fed49df55f
Anthony Kazyaka <ask.anthony@hotmail.com>
2023-04-02 00:55:08 -0400

    Add feedback component & logic

--- src/app/help-modal/help-modal.component.ts
+++ src/app/help-modal/help-modal.component.ts
@@ -1,16 +1,27 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Feedback } from '../models/Feedback';
+import { DatabaseService } from '../services/database.service';
 
 @Component({
   selector: 'app-help-modal',
   templateUrl: './help-modal.component.html',
   styleUrls: ['./help-modal.component.css']
 })
-
 export class HelpModalComponent implements OnInit {
-  constructor(@Inject(MAT_DIALOG_DATA) public helpContext: string) {}
+  constructor(@Inject(MAT_DIALOG_DATA) public helpContext: string, private databaseService: DatabaseService) {}
 
   ngOnInit(): void {
   }
 
+  submitFeedback(feedbackText: string) {
+    if (feedbackText) {
+      const feedback: Feedback = {
+        context: this.helpContext,
+        text: feedbackText
+      };
+      
+      this.databaseService.submitFeedback(feedback);
+    }
+  }  
 }
